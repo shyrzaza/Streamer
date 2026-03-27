93b6bc1ae5a2c49222da8324ab50c15f8c85387d
Cedric Fromm <cedric.fromm@corsair.com>
2026-03-27 22:33:02 +0100

    add bounds for player

--- StreamGame/player_character.gd
+++ StreamGame/player_character.gd
@@ -4,29 +4,27 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-var score = 0
+var leftSideBorder = 370
+var rightSideBorder = 800
+@export var performance = 0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	
 	var direction := Input.get_axis("ui_left", "ui_right")
-	#print(direction)
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
+	# Check boundaries
+	if position.x < leftSideBorder:
+		velocity.x = max(velocity.x, 0)
+	if position.x > rightSideBorder:
+		velocity.x = min(velocity.x, 0)
+		
 	move_and_slide()
 	
-func add_to_score(amount: int) -> void:
-	score += amount
-	
-# TODO: Setup Boundaries so the character does not leave the screen/river
+func tank_performance(amount: int) -> void:
+	print("Oh no my performance tanked")
+	performance -= amount
